53c1f99774a913c47a425b8f9cd9576c92e2a2fb
changhui98 <net_0102@naver.com>
2024-04-27 20:28:28 +0900

    2day study javascript

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
